0c8a14f1210425cfda4c265ecead24f1b7174e04
Alexandr Nechuhaev <a.nechuhaev@gmail.com>
2022-08-11 17:00:51 +0300

    task 1 html

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+console.log(createImagesCardMarkup (galleryItems));
 
-console.log(galleryItems);
+function createImagesCardMarkup (galleryItems) {
+    const markup = galleryItems.map(galleryItem => {
+        return
+        `<div class="gallery">
+    <ul>
+        <li class="gallery__item">
+            <a href="/" class="gallery__link">
+                <img class="gallery__image"></img>
+        </a>
+        </li>
+    </ul>
+</div>`;
+    });
+    console.log(markup);
+}
+ 
+// console.log(galleryItems);
+
+
+
